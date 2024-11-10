cf46bace586e64feddaf72af0777db2d6619b213
jasperin <jasperinina@mail.ru>
2024-11-10 21:45:12 +0500

    del time treker

--- lab3/lab3/Utilities/StackAnalyzer/PerformanceAnalyzer.cs
+++ lab3/lab3/Utilities/StackAnalyzer/PerformanceAnalyzer.cs
@@ -1,98 +1,99 @@
 ﻿using System.IO;
+using System.Diagnostics;
 
 namespace lab3.Utilities.StackAnalyzer;
 
 public class PerformanceAnalyzer
+{
+    private readonly string filePath;
+    private static readonly Random random = new Random();
+
+    // Конструктор с параметром для пользовательского пути или использованием пути по умолчанию
+    public PerformanceAnalyzer(string filePath = "inputStackTest.txt")
     {
-        private readonly string filePath;
-        private static readonly Random random = new Random();
+        this.filePath = filePath;
 
-        // Конструктор с параметром для пользовательского пути или использованием пути по умолчанию
-        public PerformanceAnalyzer(string filePath = "inputStackTest.txt")
+        // Генерация файла с 10,000 строк данных, если файл не существует
+        if (!File.Exists(filePath))
         {
-            this.filePath = filePath;
+            GenerateTestFile(filePath);
+        }
+    }
 
-            // Генерация файла с 10,000 строк данных, если файл не существует
-            if (!File.Exists(filePath))
-            {
-                GenerateTestFile(filePath);
-            }
+    public (double[] dataSizes, double[] times) AnalyzePerformance()
+    {
+        var dataSizes = new List<double>();
+        var times = new List<double>();
+
+        if (!File.Exists(filePath))
+        {
+            return (dataSizes.ToArray(), times.ToArray()); // Возврат пустых массивов, если файл не найден
         }
 
-        public (double[] dataSizes, double[] times) AnalyzePerformance()
+        try
         {
-            var dataSizes = new List<double>();
-            var times = new List<double>();
+            string[] lines = File.ReadAllLines(filePath);
+            var stopwatch = new Stopwatch();
 
-            if (!File.Exists(filePath))
+            foreach (var line in lines)
             {
-                return (dataSizes.ToArray(), times.ToArray()); // Возврат пустых массивов, если файл не найден
-            }
+                // Создание нового экземпляра FileHandlerStack для каждого набора данных
+                var fileHandler = new FileHandlerStack(_ => { }); // Пустой делегат для подавления вывода
 
-            try
-            {
-                string[] lines = File.ReadAllLines(filePath);
-                var timeTracker = new TimeTracker();
+                stopwatch.Restart();
+                fileHandler.ProcessLine(line);
+                stopwatch.Stop();
 
-                foreach (var line in lines)
-                {
-                    // Создание нового экземпляра FileHandlerStack для каждого набора данных
-                    var fileHandler = new FileHandlerStack(_ => { }); // Пустой делегат для подавления вывода
-
-                    timeTracker.Start();
-                    fileHandler.ProcessLine(line);
-                    timeTracker.Stop();
-
-                    dataSizes.Add(line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length);
-                    times.Add(timeTracker.GetElapsedMilliseconds());
-                }
-            }
-            catch (Exception ex)
-            {
-                // Логирование или обработка исключений при необходимости
-                Console.WriteLine($"Ошибка при анализе производительности: {ex.Message}");
-            }
-
-            return (dataSizes.ToArray(), times.ToArray());
-        }
-
-        private void GenerateTestFile(string path)
-        {
-            using (StreamWriter writer = new StreamWriter(path))
-            {
-                for (int i = 1; i <= 10000; i++)
-                {
-                    string line = GenerateRandomCommands(i);
-                    writer.WriteLine(line);
-                }
+                dataSizes.Add(line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length);
+                times.Add(stopwatch.Elapsed.TotalMilliseconds);
             }
         }
-
-        private string GenerateRandomCommands(int count)
+        catch (Exception ex)
         {
-            var commands = new List<string>();
-            for (int i = 0; i < count; i++)
-            {
-                int commandType = random.Next(1, 6); // Случайное число от 1 до 5 (включительно)
-                switch (commandType)
-                {
-                    case 1:
-                        commands.Add($"1,data{random.Next(1, 10000)}"); // Случайное значение для добавления
-                        break;
-                    case 2:
-                        commands.Add("2"); // Pop
-                        break;
-                    case 3:
-                        commands.Add("3"); // Top
-                        break;
-                    case 4:
-                        commands.Add("4"); // isEmpty
-                        break;
-                    case 5:
-                        commands.Add("5"); // Print
-                        break;
-                }
-            }
-            return string.Join(" ", commands);
+            // Логирование или обработка исключений при необходимости
+            Console.WriteLine($"Ошибка при анализе производительности: {ex.Message}");
         }
+
+        return (dataSizes.ToArray(), times.ToArray());
+    }
+
+    private void GenerateTestFile(string path)
+    {
+        using (StreamWriter writer = new StreamWriter(path))
+        {
+            for (int i = 1; i <= 10000; i++)
+            {
+                string line = GenerateRandomCommands(i);
+                writer.WriteLine(line);
+            }
+        }
+    }
+
+    private string GenerateRandomCommands(int count)
+    {
+        var commands = new List<string>();
+        for (int i = 0; i < count; i++)
+        {
+            int commandType = random.Next(1, 6); // Случайное число от 1 до 5 (включительно)
+            switch (commandType)
+            {
+                case 1:
+                    commands.Add($"1,data{random.Next(1, 10000)}"); // Случайное значение для добавления
+                    break;
+                case 2:
+                    commands.Add("2"); // Pop
+                    break;
+                case 3:
+                    commands.Add("3"); // Top
+                    break;
+                case 4:
+                    commands.Add("4"); // isEmpty
+                    break;
+                case 5:
+                    commands.Add("5"); // Print
+                    break;
+            }
+        }
+        return string.Join(" ", commands);
+    }
 }
